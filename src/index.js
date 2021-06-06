b5cc983c51902c769a5ce075844b2548598b1ef7
ubiquitouss <ubiquitouss@yahoo.com>
2021-06-06 13:16:15 +0600

    completed first project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Import the Rac and ReactDOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Create a react Component
+//it's just a function
+
+function getButtonText(first,second){
+    return first +' ' + second;
+};
+const styles ={backgroundColor:'blue',color:'white'};
+
+const App = () => {
+    //  we can also declare javascript variable and use it
+    const buttonText = 'Click Me!';
+    return (
+    <div>
+        <label className="label" htmlFor ="name">
+            Enter Name:
+        </label>
+        <input id = "name" type="text"/>
+        <button style ={styles}>{getButtonText('hello','tawkir')}</button>
+    </div>
+    );
+};
+
+//  Take the react component and show it on the screen
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>,
+    document.querySelector('#root')
+)
